bf46f0f61584ef717f5d624b5ccb2651ddd47e5c
MAGRAP <german.marzo98@gmail.com>
2023-05-15 17:38:07 +0200

    changed src

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='market-growth-analysis',
-    version='1.0.0',
+    version='1.0.2',
     packages=['market_growth_analysis'],
     package_dir={'': 'src'},
     install_requires=[
