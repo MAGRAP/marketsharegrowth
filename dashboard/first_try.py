8ae8941d4f54dbd06716c8da3382919a3cb9fa25
MAGRAP <german.marzo98@gmail.com>
2023-09-17 20:55:42 +0200

    nuevo analisis en precision

--- dashboard/first_try.py
+++ dashboard/first_try.py
@@ -1,4 +1,5 @@
 import streamlit as st
+st. set_page_config(layout="wide")
 import pandas as pd
 import numpy as np
 import yaml
@@ -22,6 +23,29 @@ with open('conf/global.yml', 'r') as f:
 with open('conf/local.yml', 'r') as f:
     credentials = yaml.safe_load(f)
 
+@st.cache_data()  # Add allow_output_mutation=True if working with mutable objects like a database connection
+def fetch_data_from_database():
+    # Create connection db
+    conn = pyodbc.connect('Driver={SQL Server};'
+                        f'Server={server};'
+                        f'Database={database};'
+                        'Trusted_Connection=yes;')
+    cursor = conn.cursor()
+
+    # Get data
+    join_tables_query = '''
+    select *
+    from DIM_COMPANY dim
+    join FACT_BALANCE_SHEET bs on bs.ticker = dim.ticker
+    join FACT_CASH_FLOW_STATEMENT cfs on cfs.PK = bs.PK
+    join FACT_INCOME_STATEMENT fis on fis.PK = bs.PK
+    join FACT_PRICES fp on fp.PK = bs.PK
+    join FACT_RATIOS fr on fr.PK = bs.PK
+    '''
+    data = pd.read_sql(join_tables_query, conn)
+
+    return data
+
 
 # Get credentials
 driver = credentials['warehouse_db']['driver']
@@ -31,54 +55,99 @@ trusted = credentials['warehouse_db']['trusted_connection']
 user = credentials['warehouse_db']['user']
 password = credentials['warehouse_db']['password']
 
-# Create connection db
-conn = pyodbc.connect('Driver={SQL Server};'
-                      f'Server={server};'
-                      f'Database={database};'
-                      'Trusted_Connection=yes;')
-cursor = conn.cursor()
+col11, col12, col13, col14 = st.columns(4)
 
-# Get data
-join_tables_query = '''
-select *
-from DIM_COMPANY dim
-join FACT_BALANCE_SHEET bs on bs.ticker = dim.ticker
-join FACT_CASH_FLOW_STATEMENT cfs on cfs.PK = bs.PK
-join FACT_INCOME_STATEMENT fis on fis.PK = bs.PK
-join FACT_PRICES fp on fp.PK = bs.PK
-join FACT_RATIOS fr on fr.PK = bs.PK
-'''
-# join_tables_query = '''
-# select *
-# from DIM_COMPANY dim
-# '''
-
-
-data = pd.read_sql(join_tables_query, conn)
-# drop duplicated columns
+# ========================= GET DATA =========================
+data = fetch_data_from_database()
 data = data.T.drop_duplicates().T
 data['Date'] = pd.to_datetime(data['Date'], format="%Y/%m/%d").dt.strftime("%Y/%m/%d")
 
+data_filtered = data[['Date', 'country', 'sector', 'industry','ticker', 'Growth +1']]
+# ========================= FILTERS =========================
+# Country
+with col11:
+    country_options = tuple(data_filtered['country'].unique())
+    country_selected = st.selectbox('Select country', country_options)
+    data_country = data_filtered[data_filtered['country'] == country_selected]
+# Sector
+with col12:
+    sector_options = tuple(data_country['sector'].unique())
+    sector_selected = st.selectbox('Select Sector', sector_options)
+    data_sector = data_country[data_country['sector'] == sector_selected]
+# Industry
+with col13:
+    industry_options = tuple(data_sector['industry'].unique())
+    industry_selected = st.selectbox('Select Industry', industry_options)
+    data_industry = data_sector[data_sector['industry'] == industry_selected]
+# Company
+with col14:
+    company_options = tuple(data_industry['ticker'].unique())
+    company_selected = st.selectbox('Select Company', company_options)
+    data_company = data_industry[data_industry['ticker'] == company_selected]
 
-# Prepare data
-company_data = data[['Date', 'ticker', 'Growth +1']].sort_values(by=['ticker', 'Date']).reset_index(drop=True)
-categories_data = data[['ticker', 'industry', 'sector']].drop_duplicates()
-st.write(company_data.head())
 
-# Pivot the data
-pivoted_data = company_data.pivot(index='ticker',columns='Date', values='Growth +1')
-st.write(pivoted_data.head())
+# ========================= PREPARE DATA =========================
+# all_data = data_filtered[['Date','country', 'Growth +1']].groupby(by=['Date','country']).median().sort_values(by=['Date', 'country']).reset_index()
+country_data = data_country[['Date','country', 'Growth +1']].groupby(by=['Date','country']).median().sort_values(by=['Date', 'country']).reset_index()
+sector_data = data_sector[['Date','sector', 'Growth +1']].groupby(by=['Date','sector']).median().sort_values(by=['Date', 'sector']).reset_index()
+industry_data = data_industry[['Date', 'industry', 'Growth +1']].groupby(by=['Date','industry']).median().sort_values(by=['Date', 'industry']).reset_index()
+company_data = data_company[['Date', 'ticker', 'Growth +1']].sort_values(by=['Date', 'ticker']).reset_index(drop=True)
+
+# categories
+# categories_data = data_industry[['ticker', 'industry', 'sector']].drop_duplicates()
 
 
-# Plot the data
-plt.figure(figsize=(12, 6))  # Set the size of the plot
-for column in pivoted_data.columns:
-    plt.plot(pivoted_data[column], label=column)
+# Pivot country data
+pivoted_country_data = country_data.pivot(index='Date',columns='country', values='Growth +1')
+pivoted_country_data.index= pd.to_datetime(pivoted_country_data.index, format="%Y/%m/%d")
+# Pivot sector data
+pivoted_sector_data = sector_data.pivot(index='Date',columns='sector', values='Growth +1')
+pivoted_sector_data.index= pd.to_datetime(pivoted_sector_data.index, format="%Y/%m/%d")
+# Pivot industry data
+pivoted_industry_data = industry_data.pivot(index='Date',columns='industry', values='Growth +1')
+pivoted_industry_data.index= pd.to_datetime(pivoted_industry_data.index, format="%Y/%m/%d")
+# Pivot ticker data
+pivoted_company_data = company_data.pivot(index='Date',columns='ticker', values='Growth +1')
+pivoted_company_data.index= pd.to_datetime(pivoted_company_data.index, format="%Y/%m/%d")
 
-plt.xlabel('Time')  # Set the x-axis label
-plt.ylabel('Year Growth')  # Set the y-axis label
-plt.title('Year Growth Evolution')  # Set the title of the plot
-plt.legend()  # Show the legend
-plt.xticks(rotation=45)  # Rotate x-axis tick labels for better readability
-plt.tight_layout()  # Adjust the layout
-plt.show()  # Display the plot
+# ========================= PLOT DATA =========================
+# Plot each ticker as a separate line
+
+def plot_line_plot(pivoted_data):
+    plt.figure(figsize=(10, 6))  # Adjust the figure size as needed
+    for column in pivoted_data.columns:
+        filtered_data = pivoted_data.dropna(subset=[column])
+        plt.plot(filtered_data.index, filtered_data[column], marker='o', label=column)
+
+
+    # Customize the plot
+    plt.xlabel('Date')
+    plt.ylabel('Value')
+    plt.title('Line Plot for Tickers')
+    plt.grid(True)
+    plt.legend()
+
+    # Rotate the x-axis labels
+    plt.xticks(rotation=45)
+
+    # Display the plot using Streamlit
+    st.set_option('deprecation.showPyplotGlobalUse', False) # Avoid Warning
+    st.pyplot()
+
+    return
+
+col21, col22, col23, col24 = st.columns([0.25, 0.25, 0.25, 0.25]) 
+# Country
+with col21:
+    plot_line_plot(pivoted_country_data)
+# Sector
+with col22:
+    plot_line_plot(pivoted_sector_data)
+# Industry
+with col23:
+    plot_line_plot(pivoted_industry_data)
+# Company
+with col24:
+    plot_line_plot(pivoted_company_data)
+
+
